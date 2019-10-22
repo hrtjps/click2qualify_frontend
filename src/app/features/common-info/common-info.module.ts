2ae31840d3b443c9ede9da956b93394515596ae8
JPerfectSolution <j.perfectsolution@outlook.com>
2019-10-22 14:35:54 +0400

    add contact us page

--- src/app/features/common-info/common-info.module.ts
+++ src/app/features/common-info/common-info.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 
 
 @NgModule({
-  declarations: [ProfileComponent],
+  declarations: [ProfileComponent, ContactUsComponent],
   imports: [
     CommonModule,
     SharedModule,
@@ -17,6 +18,11 @@ import { RouterModule } from '@angular/router';
         path: 'profile',
         component: ProfileComponent,
         data: {breadcrumbs: ['Common Info', 'User Profile']}
+      },
+      {
+        path: 'contact-us',
+        component: ContactUsComponent,
+        data: {breadcrumbs: ['Common Info', 'Contact Us']}
       },
     ])
   ]
